b3a7d857b6a3c73c2b827995e57c79d2a4ec4451
yogaibnu <yogaibnudinataa@gmail.com>
2021-01-05 10:21:35 +0700

    add create/new

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,14 @@
     use App\Entity\Article;
 
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
     class ArticleController extends AbstractController {
         /**
@@ -19,6 +25,31 @@
             return $this->render('article/article.html.twig', array
                 ('article' => $article)
             );
+        }
+
+        /**
+         * @Route("/article/new", name="new_article")
+         * Method({"GET", "POST"})
+         */
+        public function new(Request $request) {
+            $article = new Article();
+
+            $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' =>
+            array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr'  => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+            return $this->render('article/new.html.twig', array(
+                'form' => $form->createView()
+            ));
         }
 
         /**
